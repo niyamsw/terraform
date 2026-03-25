2c6380d6f9799090cdaf66ea47c6eb8bdea7c623
Sarah French <sarah.french@hashicorp.com>
2026-03-25 18:32:45 +0000

    refactor: Make `workspace list` command parse its arguments using the `arguments` package

---
+++ internal/command/arguments/workspace.go
@@ -0,0 +1,41 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+package arguments
+
+import (
+	"github.com/hashicorp/terraform/internal/tfdiags"
+)
+
+// Workspace represents the command-line arguments common between all workspace subcommands.
+//
+// Subcommands that accept additional arguments should have a specific struct that embeds this struct.
+type Workspace struct {
+	// ViewType specifies which output format to use
+	ViewType ViewType
+}
+
+// ParseWorkspace processes CLI arguments, returning a Workspace value and errors.
+// If errors are encountered, an Workspace value is still returned representing
+// the best effort interpretation of the arguments.
+func ParseWorkspace(args []string) (*Workspace, tfdiags.Diagnostics) {
+	var diags tfdiags.Diagnostics
+
+	var jsonOutput bool
+	cmdFlags := defaultFlagSet("workspace list")
+	cmdFlags.BoolVar(&jsonOutput, "json", false, "produce JSON output")
+	if err := cmdFlags.Parse(args); err != nil {
+		diags = diags.Append(tfdiags.Sourceless(
+			tfdiags.Error,
+			"Failed to parse command-line flags",
+			err.Error(),
+		))
+	}
+
+	switch {
+	case jsonOutput:
+		return &Workspace{ViewType: ViewJSON}, diags
+	default:
+		return &Workspace{ViewType: ViewHuman}, diags
+	}
+}
